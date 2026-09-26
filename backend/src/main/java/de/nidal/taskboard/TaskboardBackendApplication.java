0dc34c062132b46ab908daaf86d9c84d568f82a3
Nidal Ayadi <147609483+Nidal1211@users.noreply.github.com>
2026-09-26 02:15:22 +0200

    Initialisiere das Backend-Projekt mit Maven, füge PostgreSQL-Datenbankkonfiguration hinzu.

---
+++ backend/src/main/java/de/nidal/taskboard/TaskboardBackendApplication.java
@@ -0,0 +1,13 @@
+package de.nidal.taskboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskboardBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/nidal/taskboard/TaskboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nidal.taskboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
